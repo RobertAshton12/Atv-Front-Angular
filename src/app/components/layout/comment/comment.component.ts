902088925d0b40cfa696dc1cebe7ee05c02ae9d3
RobertAshton12 <kookieplays02@gmail.com>
2025-04-28 21:46:33 -0300

    asdf

--- src/app/components/layout/comment/comment.component.ts
+++ src/app/components/layout/comment/comment.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { CommentService } from '../../../service/comment.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-comment',
@@ -17,7 +19,14 @@ export class CommentComponent {
   rating: number = 0;         
   hoverRating: number = 0;    
 
-  constructor() {}
+  commentService=inject(CommentService);
+  router=inject(Router);
+
+  comments: any[] = []; 
+  constructor() {
+    this.findAll();
+
+  }
   addComment(): void {
     if (!this.nameComment || !this.text) {
       Swal.fire('Preencha todos os campos', '', 'warning');
@@ -29,14 +38,21 @@ export class CommentComponent {
       text: this.text,
       date: new Date()
     };
-  
    
     console.log('Comentário adicionado:', newComment);
   
-    // Limpa os campos
     this.nameComment = '';
     this.text = '';
   }
+
+  async findAll() {
+    try {
+      this.comments = await this.commentService.findAll();
+    } catch (erro) {
+      Swal.fire('Erro ao buscar comentários', '', 'error');
+    }
+  }
+
   setRating(star: number): void {
     this.rating = star;
   }
